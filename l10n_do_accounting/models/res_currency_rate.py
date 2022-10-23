78269a818a146d28104a9b8548e386175373bdb7
alexcape14 <acapellan@capw.com.do>
2022-10-23 10:22:10 -0400

    [FIX] - Correccion del calculo de la tasa. [UPDATE] - Actualizando las traducciones y comentando los cambios de tasa financieras manuales para trabajo futuro
    Signed-off-by: alexcape14 <acapellan@capw.com.do>

--- l10n_do_accounting/models/res_currency_rate.py
+++ l10n_do_accounting/models/res_currency_rate.py
@@ -5,21 +5,16 @@ class ResCurrencyRate(models.Model):
     _inherit = 'res.currency.rate'
     _description = 'Description'
 
-    @api.depends("rate")
+    converted = fields.Float(readonly=False, digits=(2, 2))
+
+    @api.onchange("converted")
     def _get_converted(self):
-        for rec in self:
-            if rec.rate > 0:
-                rec.converted = 1 / rec.rate
+        if self.converted > 0:
+            self.rate = 1 / self.converted
 
-    converted = fields.Float(compute=_get_converted, readonly=True, digits=(12, 12))
-
-    def name_get(self):
-        result = []
-        for rate in self:
-            result.append(
-                (rate.id, "{} | Tasa: {}".format(rate.name, rate.converted)))
-        return result
-
-    rate = fields.Float(
-        digits=(2, 2),
-        help='The rate of the currency to the currency of rate 1')
+    # def name_get(self):
+    #     result = []
+    #     for rate in self:
+    #         result.append(
+    #             (rate.id, "{} | Tasa: {}".format(rate.name, rate.converted)))
+    #     return result

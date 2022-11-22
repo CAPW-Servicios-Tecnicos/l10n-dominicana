e261b7f58df41000afc4be00824dfe54bede4eec
cnune <christopheraned@gmail.com>
2022-11-22 16:46:33 -0400

    [ADD] SobreEscribiendo metodo de crear factura en Purchase Order, [ADD] depends in purchase

--- l10n_do_accounting/__manifest__.py
+++ l10n_do_accounting/__manifest__.py
@@ -10,7 +10,7 @@
     "website": "https://github.com/odoo-dominicana",
     "version": "14.0.2.12.18",
     # any module necessary for this one to work correctly
-    "depends": ["l10n_latam_invoice_document", "l10n_do"],
+    "depends": ["l10n_latam_invoice_document", "l10n_do", 'purchase'],
     # always loaded
     "data": [
         "security/ir.model.access.csv",

708db486b4726b14d5161b9cfa8337546c6ad967
Alejandro Capellan <acapellan@capw.com.do>
2023-01-25 01:04:55 -0400

    [IMP] agregando y dividiendo nuevo modelo de purchase order para localizacion
    Signed-off-by: Alejandro Capellan <acapellan@capw.com.do>

---
+++ l10n_do_purchase/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Fiscal Purchase',
+    'version': '13.0.1',
+    'summary': 'Summery',
+    'description': 'Description',
+    'category': 'Localization',
+    'author': 'CAPW Servicios Tecnicos',
+    'website': 'https://capw.com.do',
+    'license': 'LGPL-3',
+    'depends': ['l10n_do_accounting', 'purchase'],
+    'installable': False,
+    'auto_install': False
+}

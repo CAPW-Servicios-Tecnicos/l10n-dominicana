db8d77c573e1188a9b5d34ea1c4a7b844fb9cc8f
cnune <cnunez@capw.com.do>
2023-10-19 18:41:19 -0400

    [CHANGE] NUEVOS CAMBIOS A Autoparts,crm_service 19/10/2023
    MEJORANDO EL MODULO DE AUTOPARTS, Y creando la integracion con servicios externos a hoja de trabajo

--- l10n_do_accounting/models/account_move.py
+++ l10n_do_accounting/models/account_move.py
@@ -2,6 +2,7 @@ import re
 from psycopg2 import sql
 from werkzeug import urls
 import json
+from lxml import etree
 from odoo import models, fields, api, _
 from odoo.osv import expression
 from odoo.exceptions import ValidationError, UserError, AccessError
@@ -417,6 +418,23 @@ class AccountMove(models.Model):
             raise ValidationError(
                 _("Duplicated vendor reference detected. You probably encoded twice the same vendor bill/credit note: "
                   "%s") % self.l10n_latam_document_number)
+
+    # @api.model
+    # def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
+    #     res = super(AccountMove, self).fields_view_get(view_id, view_type, toolbar=toolbar, submenu=False)
+    #     if view_type == 'form':
+    #         xml_arch = etree.XML(res['arch'])
+    #         line_fields = xml_arch.xpath("//field[@name='l10n_latam_document_number']")
+    #         move_fields = xml_arch.xpath("//field[@name='move_type']")[0]
+    #         print("view_id....", xml_arch.Element('move_type'))
+    #         for node in line_fields:
+    #             # modifiers = {
+    #             #     'invisible': ['|', '|', ('country_code', '=', 'DO'), ('state', '=', 'cancel'),
+    #             #                   ('move_type', 'in', ('out_invoice', 'out_refund'))], }
+    #             modifiers = {}
+    #             node.set("modifiers", json.dumps(modifiers))
+    #         res['arch'] = etree.tostring(xml_arch, encoding='unicode')
+    #     return res
 
     @api.depends("company_id", "company_id.l10n_do_ecf_issuer")
     def _compute_company_in_contingency(self):

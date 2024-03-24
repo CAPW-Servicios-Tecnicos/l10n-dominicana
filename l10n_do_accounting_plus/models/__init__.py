3a049825cc64dd9caa821f8cc992729b660e73ab
Alejandro Capellan <acapellan@capw.com.do>
2024-03-24 10:18:46 -0400

    Delete l10n_do_accounting_plus directory

--- l10n_do_accounting_plus/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import wsmovildgii
-from . import account_journal
-from . import res_company
-from . import res_config_setting
-from . import account_move
-from . import account_payment
-from . import res_partner

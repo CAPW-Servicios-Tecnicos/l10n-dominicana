8a97a282feb2e945494e726680008a6bcaaf9eff
Alejandro Capellan <acapellan@capw.com.do>
2024-05-05 10:26:12 -0400

    [UPD] actualizacion l10n_do_dgii_tax_payer_type se quita que sea requerido directo por el campo.

--- l10n_do_accounting/models/res_partner.py
+++ l10n_do_accounting/models/res_partner.py
@@ -41,7 +41,6 @@ class Partner(models.Model):
         string="Taxpayer Type",
         index=True,
         store=True,
-        required=True,
     )
     l10n_do_expense_type = fields.Selection(
         selection="_get_l10n_do_expense_type",
